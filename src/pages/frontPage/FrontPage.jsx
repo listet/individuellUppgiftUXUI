ab0e18495f75b7fce406d6bebd3d81f58669089a
Lisa Tetzlaff <lisa.emma.tetzlaff@gmail.com>
2024-10-23 21:48:33 +0200

    pulse frontPage

--- src/pages/frontPage/FrontPage.jsx
+++ src/pages/frontPage/FrontPage.jsx
@@ -1,13 +1,16 @@
+import { section } from 'framer-motion/client'
 import logo from '../../assets/logo.svg'
 import './frontPage.css'
 import { Link } from 'react-router-dom'
 
 function FrontPage() {
     return (
-        <Link aria-label='Navigate to timePage' className='frontPage-wrapper' to='/TimerPage'>
-            <img src={logo} alt="logo" />
-            <p className='frontPage-text'>For all your timing needs</p>
-        </Link>
+        <section className='frontPage-wrapper'>
+            <Link aria-label='Navigate to timePage' className='logo-wrapper' to='/TimerPage'>
+                <img src={logo} alt="logo" />
+                <p className='frontPage-text'>For all your timing needs</p>
+            </Link>
+        </section>
     )
 }
 
